b747a4e5a75996d4bcb85a09bde4243290d780ce
Ayhan <ayhansiami@gmail.com>
2026-07-15 13:37:03 +0330

    Implement Add Password feature with file storage.

--- main.py
+++ main.py
@@ -17,21 +17,21 @@ Choose an option: ''')
 Which website do you want to set a password for?
 Available Websites:
 1. MoonKing.net (1)
-2. chatgooz.tv (2)
-3. ko3nanat.com (3)
+2. chatbot.tv (2)
+3. ayhan.com (3)
 ''')
             if website == '1':
                 website_choice = 'MoonKing.net'
                 print('Website: MoonKing.net')
                 break
             elif website == '2':
-                website_choice = 'chatgooz.tv'
-                print('Website: chatgooz.tv')
+                website_choice = 'chatbot.tv'
+                print('Website: chatbot.tv')
                 break
             elif website == '3':
-                #I defined this (website_choice) variable because you said you should not change the user input.
-                website_choice = 'ko3nanat.com'
-                print('Website: ko3nanat.com')
+                # I defined this (website_choice) variable because you said you should not change the user input.
+                website_choice = 'ayhan.com'
+                print('Website: ayhan.com')
                 break
             else:
                 print('I didn\'t get that, Please try agian...')
@@ -41,6 +41,17 @@ Available Websites:
             password_input2 = input('Enter your password agian: ')
             if password_input1 == password_input2:
                 main_password = password_input1  # just for being clean for later
+                account_info = (f'''-------------------------------------
+
+Website: {website_choice}
+Username: {user_name}
+Password: {main_password}
+
+-------------------------------------
+''')
+                storage = open('passwords.txt', 'a')
+                storage.write(account_info)
+                storage.close()
                 print(f'''
 You have succesfully difined a new Password for \'{website_choice}\' website,
 Website: \'{website_choice}\'
